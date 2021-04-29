93baf29a371c2bfe7ec9cebc4ca4135b6aec2d4a
Lubinl <lovenskyblubin@gmail.com>
2021-04-29 14:40:07 -0400

    Corrected Loop and formula

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,9 +4,13 @@ import csv
 #Variables
 month_count = 0
 previous_profit = 0
+totalchange = 0
+maxinc = 0
+maxdec = 0
 months = []
 profitlosses = []
-
+maxincdate = ''
+maxdecdate = ''
 #Read CSV file
 with open(os.path.join('Resources','budget_data.csv'), 'r') as csvfile:
  # Split the data on commas
@@ -14,16 +18,33 @@ with open(os.path.join('Resources','budget_data.csv'), 'r') as csvfile:
    columns_names = next(csvreader)
    for row in csvreader:
       month_count = month_count + 1
-      profit = row[1]
-      month = row[0]
-      months.append(month)
+      profit = int(row[1])
+      if month_count > 1:
+         change = profit - previous_profit
+         totalchange = totalchange + change
+         if change > maxinc:
+            maxinc = change
+            maxincdate = row[0]
+         if change < maxdec:
+            maxdec = change
+            maxdecdate = row[0]
       profitlosses.append(int(profit))
-avg_change = sum(profitlosses) / month_count
+      previous_profit = profit
+avg_change = totalchange / (month_count-1)
 print(month_count)
 print(sum(profitlosses))
 print(round(avg_change,2))
-print(max(profitlosses))
-print(min(profitlosses))
-max_index = profitlosses.index(max(profitlosses))
-print(max_index)
-print(months[max_index])
+print(maxinc)
+print(maxincdate)
+print(maxdec)
+print(maxdecdate)
+
+  ```text
+  Financial Analysis
+  ----------------------------
+  Total Months: 86
+  Total: $38382578
+  Average  Change: $-2315.12
+  Greatest Increase in Profits: Feb-2012 ($1926159)
+  Greatest Decrease in Profits: Sep-2013 ($-2196167)
+  ```
