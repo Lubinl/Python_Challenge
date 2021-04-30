e31bc975a947dc1fb522e5eb6e24ff45af741125
Lubinl <lovenskyblubin@gmail.com>
2021-04-29 20:33:54 -0400

    Corrected code to generate outputs and text files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,13 @@
+#import packages
 import os
 import csv
 
-#Variables
+#Creation of Variables/List/Value Holders
 month_count = 0
 previous_profit = 0
 totalchange = 0
 maxinc = 0
 maxdec = 0
-months = []
 profitlosses = []
 maxincdate = ''
 maxdecdate = ''
@@ -16,9 +16,13 @@ with open(os.path.join('Resources','budget_data.csv'), 'r') as csvfile:
  # Split the data on commas
    csvreader = csv.reader(csvfile, delimiter=',')
    columns_names = next(csvreader)
+   #Loop throughfile
    for row in csvreader:
+      #Total number of months in file
       month_count = month_count + 1
+      #Identify locaiton of profit values
       profit = int(row[1])
+      #Conditonal statement
       if month_count > 1:
          change = profit - previous_profit
          totalchange = totalchange + change
@@ -28,23 +32,22 @@ with open(os.path.join('Resources','budget_data.csv'), 'r') as csvfile:
          if change < maxdec:
             maxdec = change
             maxdecdate = row[0]
+      #Add profit values to Profit/Loss list
       profitlosses.append(int(profit))
       previous_profit = profit
+#Formula used to identify average change in profit
 avg_change = totalchange / (month_count-1)
-print(month_count)
-print(sum(profitlosses))
-print(round(avg_change,2))
-print(maxinc)
-print(maxincdate)
-print(maxdec)
-print(maxdecdate)
-
-  ```text
-  Financial Analysis
-  ----------------------------
-  Total Months: 86
-  Total: $38382578
-  Average  Change: $-2315.12
-  Greatest Increase in Profits: Feb-2012 ($1926159)
-  Greatest Decrease in Profits: Sep-2013 ($-2196167)
-  ```
+#Create list to collect output
+results = []
+results.append(f'Financial Analysis')
+results.append(f'----------------------------')
+results.append(f'Total Months: {month_count}')
+results.append(f'Total: ${sum(profitlosses)}')
+results.append(f'Average  Change: ${round(avg_change,2)}')
+results.append(f'Greatest Increase in Profits: {maxincdate} (${maxinc})')
+results.append(f'Greatest Decrease in Profits: {maxdecdate} (${maxdec})')
+#Write output into textfile using loop
+with open(os.path.join('Analysis','Budget Data Analysis.txt'), 'w') as txtfile:
+  for result in results:
+    print(result)
+    txtfile.write(result + '\n')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,10 @@
+#import packages
+import os
+import csv
+#creation of variables/lists/Value Holder
 total_votes = 0
 votes = []
 candidates = []
-
 #Read in the CSV file
 with open(os.path.join('Resources','election_data.csv'), 'r') as csvfile:
 
@@ -23,35 +26,32 @@ with open(os.path.join('Resources','election_data.csv'), 'r') as csvfile:
         else:
           candidates.append(candidate)
           votes.append(1)
-
  #Election Metrics
 candidate_dist_of_votes = []
 total_tally = 0
 total_number_of_votes = votes[0]
-
-  #  
+#Loop through numbers in candidadates list
 for vote_tally in range(len(candidates)):
+  #Formula to identify candidate voter distribution
   vote_dist = votes[vote_tally]/total_votes*100
   candidate_dist_of_votes.append(vote_dist)
+  #Loop used to identify 
   if votes[vote_tally]> total_number_of_votes:
     total_number_of_votes = votes[vote_tally]
     total_tally = vote_tally
+#Identify winner of election
 winner = candidates[total_tally]
-  #
-  #
+#Round percentages
 candidate_dist_of_votes = [round(i,2) for i in candidate_dist_of_votes]
-
+#Collect output into results
 results = []
 results.append(f"Election Results\n-------------------------")
 results.append(f"Total Votes: {total_votes}\n-------------------------")
-
 for vote_tally in range(len(candidates)):
-  print(f"{candidates[vote_tally]}: {candidate_dist_of_votes[vote_tally]}% ({votes[vote_tally]})")
-  results.append(f"{candidates[vote_tally]}: {candidate_dist_of_votes[vote_tally]}% ({votes[vote_tally]})")
-
-results.append(f"-------------------------\n Winner: {winner}\n-------------------------")
-
-
+  print(f"{candidates[vote_tally]}: {candidate_dist_of_votes[vote_tally]}00% ({votes[vote_tally]})")
+  results.append(f"{candidates[vote_tally]}: {candidate_dist_of_votes[vote_tally]}00% ({votes[vote_tally]})")
+results.append(f"-------------------------\nWinner: {winner}\n-------------------------")
+#Write output into text file with use of loop
 with open(os.path.join('Analysis','Election_Analysis.txt'), 'w') as txtfile:
   for result in results:
     print(result)
